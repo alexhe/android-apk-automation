51dcefc3299de9514b1d22121f00bc243a583d6c
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2014-08-07 14:51:16 +0100

    linpack: added automation for linpack standard
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

---
+++ linpack/vc.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import os
+import time
+from subprocess import call
+
+from com.dtmilano.android.viewclient import ViewClient, ViewNotFoundException
+
+kwargs1 = {'verbose': False, 'ignoresecuredevice': False}
+device, serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+kwargs2 = {'startviewserver': True, 'forceviewserveruse': False, 'autodump': False, 'ignoreuiautomatorkilled': True}
+vc = ViewClient(device, serialno, **kwargs2)
+
+time.sleep(2)
+vc.dump(window='-1')
+start_single_button = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/btnsingle")
+start_single_button.touch()
+time.sleep(2)
+vc.dump(window='-1')
+mflops_single_score = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/txtmflops_result")
+time_single_score = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/txttime_result")
+
+call(['lava-test-case', '"Linpack MFLOPS Single Score"', '--result pass', '--measurement', mflops_single_score.getText(), '--units MFLOPS'])
+call(['lava-test-case', '"Linpack Time Single Score"', '--result pass', '--measurement', time_single_score.getText(), '--units seconds'])
+
+start_multi_button = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/btncalculate")
+start_multi_button.touch()
+time.sleep(2)
+vc.dump(window='-1')
+mflops_multi_score = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/txtmflops_result")
+time_multi_score = vc.findViewByIdOrRaise("com.greenecomputing.linpack:id/txttime_result")
+
+call(['lava-test-case', '"Linpack MFLOPS Multi Score"', '--result pass', '--measurement', mflops_multi_score.getText(), '--units MFLOPS'])
+call(['lava-test-case', '"Linpack Time Multi Score"', '--result pass', '--measurement', time_multi_score.getText(), '--units seconds'])
