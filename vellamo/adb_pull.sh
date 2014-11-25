b76acbc7564fc6724fe994633191bdcd06436152
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2014-11-25 15:00:51 +0000

    vellamo: fixed shell script
    Also added dependency for parsing vellamo results on the host side.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- vellamo/adb_pull.sh
+++ vellamo/adb_pull.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #Required for N7 even after rooting 
 adb shell chmod 777 /data/data/com.quicinc.vellamo/files/latest_result.html
@@ -9,3 +9,4 @@ adb pull /data/data/com.quicinc.vellamo/files/latest_result.html .
 if [ $? -ne 0 ]; then
     echo "Failed to get the result of vellamo test"
     exit 1
+fi
