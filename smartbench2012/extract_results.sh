d138caf037b1894a5cb31c08b399afd84d4c2cdd
Botao Sun <botao.sun@linaro.org>
2015-01-23 10:31:29 +1100

    Add Default Measurement Unit for SmartBench 2012.
    Using "points" as the default measurement unit for SmartBench 2012 Test.
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: I3060db1c3be2a48c9066828db690da643b0280d0

--- smartbench2012/extract_results.sh
+++ smartbench2012/extract_results.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 SCORE=`awk -F'[][]' '/SBGlobal.nProductivityIndex/{k=$2}END{print k}' logcat.log`
+UNIT="points"
+
 echo "Score is: $SCORE"
-lava-test-case SmartbenchScore --result pass --measurement $SCORE
+lava-test-case SmartbenchScore --result pass --measurement $SCORE --units $UNIT
